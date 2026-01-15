388b550645466094f0b27c500f75fe8e464747f2
Andrew Tyler <drewstert@outlook.com>
2026-01-15 09:30:06 -0600

    Conflict Test

--- c/lab1/main.c
+++ c/lab1/main.c
@@ -35,8 +35,8 @@ int main() {
     printf("--- Part 1: State and Sequence ---\n");
 
     int x = 2;
-    x = x + 3;
-    x = x * 4;
+    x = x + 4;
+    x = x * 3;
     printf("x=%d\n", x);
 
     // TODO: swap the last two assignments above and re-run
